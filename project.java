aa824b0f6dc1e2a97c9be083f00ab8daf8603c9f
GabrielLee <xienli1025@gmail.com>
2024-05-12 20:46:33 +0800

    1

--- project.java
+++ project.java
@@ -11,7 +11,7 @@ public class project {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         JButton btn = new JButton("Click me!");
-        JButton btn1 = new JButton("skrskrskr????");
+        JButton btn1 = new JButton("skrskrskr");
         MyButtonListener mblistener = new MyButtonListener();
         btn.addActionListener(mblistener);
         btn.setLocation(250, 100);
