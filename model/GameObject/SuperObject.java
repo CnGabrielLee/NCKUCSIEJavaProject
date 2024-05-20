c22613fea1bc4ff783555db67990549b6baa04f0
GabrielLee <xienli1025@gmail.com>
2024-05-20 20:52:29 +0800

    add model/GameObject/SuperObject

---
+++ model/GameObject/SuperObject.java
@@ -0,0 +1,15 @@
+package model.GameObject;
+
+public abstract class SuperObject {
+    private int x;
+    private int y;
+    private int width;
+    private int height;
+
+    public SuperObject(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
