48ac6bb674c5cea2c32aca4d164ca45e74c8f197
GabrielLee <xienli1025@gmail.com>
2024-05-20 21:39:39 +0800

    update MapObject

--- model/GameObject/MapObject.java
+++ model/GameObject/MapObject.java
@@ -1,5 +1,22 @@
 package model.gameobject;
 
-public class MapObject {
-    
+import java.awt.*;
+import javax.swing.*;
+public class MapObject extends SuperObject{
+    private ImageIcon img;
+    private int sx1, sy1, sx2, sy2;
+    public MapObject(int x, int y, int width, int height, int sx1, int sy1, int sx2, int sy2, ImageIcon img) {
+        super(x, y, width, height);
+        this.sx1 = sx1;
+        this.sy1 = sy1;
+        this.sx2 = sx2;
+        this.sy2 = sy2;
+
+    }
+    public void showObject(Graphics g){
+        g.drawImage(img.getImage(), sx1, sy1, null);
+    }
+    public ImageIcon getimg(){
+        return img;
+    }
 }
